7e03395005f77f2ac6b6d4230a3d24f4132a7c72
Andrei Paleyes <apaleyes@users.noreply.github.com>
2014-01-18 10:44:12 +0000

    Refactoring canvas handling by introducing CanvasManager. Modifying juliaSets to use CanvasManager

---
+++ source/CanvasManager.js
@@ -0,0 +1,100 @@
+var CanvasManager = function (canvasId, defaultAxes, checkPoint) {
+    this.canvasId = canvasId;
+    this.checkPoint = checkPoint;
+
+    this.canvas = document.getElementById(this.canvasId);
+    this.canvasRect = this.canvas.getBoundingClientRect();
+    this.context = this.canvas.getContext('2d');
+
+    this.defaultAxes = defaultAxes;
+    this.currentAxes = this.defaultAxes;
+
+    // init zoom
+    this.startX = 0;
+    this.startY = 0;
+    this.endX = 0;
+    this.endY = 0;
+    this.isZoomInProcess = false;
+    this.initStartZoom();
+    this.initEndZoom();
+}
+
+CanvasManager.prototype = {
+    translateX: function (px) {
+        var x_range = this.currentAxes.x_max - this.currentAxes.x_min;
+        var x = x_range / this.canvas.width * px + this.currentAxes.x_min;
+        return x;
+    },
+
+    translateY: function (py) {
+        var y_range = this.currentAxes.y_max - this.currentAxes.y_min;
+        var y = -1.0 * y_range / this.canvas.height * py + this.currentAxes.y_max;
+        return y;
+    },
+
+    translatePoint: function (px, py) {
+        var x = this.translateX(px);
+        var y = this.translateY(py);
+
+        return new Complex(x, y);
+    },
+
+    clearCanvas: function () {
+        this.context.fillStyle = "#FFFFFF";
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+
+    drawSet: function () {
+        this.context.fillStyle = "#000000";
+        for (var w = 0; w <= this.canvas.width; w++){
+            for (var h = 0; h <= this.canvas.height; h++){
+                var z = this.translatePoint(w, h);
+                if (this.checkPoint(z)) {
+                    // belongs to set - color black                
+                    //ctx.putImageData(pixelData, w, h);
+                    this.context.fillRect(w, h, 1, 1);
+                } else {
+                    // does not belong to set - do nothing (color white)
+                }            
+            }
+        }
+    },
+
+    initStartZoom: function() {
+        var self = this;
+        self.canvas.onmousedown = function (e) {
+            self.startX = (e.clientX - self.canvasRect.left) / self.canvas.clientWidth * self.canvas.width;
+            self.startY = (e.clientY - self.canvasRect.top) / self.canvas.clientHeight * self.canvas.height;
+            self.isZoomInProcess = true;
+        }
+    },
+
+    initEndZoom: function() {
+        var self = this;
+        self.canvas.onmouseup = function (e) {
+            if (!self.isZoomInProcess) {
+                return;
+            }
+
+            self.endX = (e.clientX - self.canvasRect.left) / self.canvas.clientWidth * self.canvas.width;
+            self.endY = (e.clientY - self.canvasRect.top) / self.canvas.clientHeight * self.canvas.height;
+
+            self.performZoom();
+
+            self.isZoomInProcess = false;
+        }
+    },
+
+    performZoom: function() {
+        this.currentAxes = {
+            x_min: this.translateX(Math.min(this.startX, this.endX)),
+            x_max: this.translateX(Math.max(this.startX, this.endX)),
+            y_min: this.translateY(Math.max(this.startY, this.endY)),
+            y_max: this.translateY(Math.min(this.startY, this.endY))
+        }
+
+        //maxIter *= 2;
+        this.clearCanvas();
+        this.drawSet(self.checkPoint);
+    }
+}
--- source/juliaSet.js
+++ source/juliaSet.js
@@ -1,48 +1,20 @@
 window.onload = function (){
     var maxIter = 20;
 
-    var c = new Complex(1/3, 1/3);
+    var c = new Complex(1/3, -1/2);
     function f(z) {
         return z.square().add(c);
     }
     var R = (1.0 + Math.sqrt(1 + 4 * c.abs())) / 2.0;
 
-    var canvas = document.getElementById('main');
-    var canvasRect = canvas.getBoundingClientRect();
-    var ctx = canvas.getContext('2d');
     var defaults = {
        x_min: -R,
        x_max: R,
        y_min: -R,
        y_max: R 
     };
-    var x_min = defaults.x_min, x_max = defaults.x_max, y_min = defaults.y_min, y_max = defaults.y_max;
-    var width = canvas.width, height = canvas.height;
 
-    function translateX(px, boundaries) {
-        var x_range = boundaries.x_max - boundaries.x_min;
-        // var px_shift = width * Math.abs(boundaries.x_min) / x_range; // shift to center for canvas coordinates
-        // var x = (px - px_shift) * x_range / width;
-        var x = x_range / width * px + boundaries.x_min;
-        return x;
-    }
-
-    function translateY(py, boundaries) {
-        var y_range = boundaries.y_max - boundaries.y_min;
-        // var py_shift = height * Math.abs(boundaries.y_min) / y_range;
-        // var y = -1 * (py - py_shift) * y_range / height;
-        var y = -1.0 * y_range / height * py + boundaries.y_max;
-        return y;
-    }
-
-    function translatePoint(px, py, boundaries) {
-        var x = translateX(px, boundaries);
-        var y = translateY(py, boundaries);
-
-        return new Complex(x, y);
-    }
-
-    function checkPoint(z) {
+    function checkJuliaPoint(z) {
         for (var i = 1; i <= maxIter; i++) {
             z = f(z);
             if (z.abs() > R) return false;
@@ -50,71 +22,6 @@ window.onload = function (){
         return true;
     }
 
-    function clearCanvas() {
-        ctx.fillStyle = "#FFFFFF";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-    }
-
-    function drawSet() {
-        ctx.fillStyle = "#000000";
-        for (var w = 0; w <= width; w++){
-            for (var h = 0; h <= height; h++){
-                var z = translatePoint(w, h, {x_min:x_min, x_max:x_max, y_min:y_min, y_max:y_max});
-                if (checkPoint(z)) {
-                    // belongs to set - color black                
-                    //ctx.putImageData(pixelData, w, h);
-                    
-                    ctx.fillRect(w, h, 1, 1);
-                } else {
-                    // does not belong to set - do nothing (color white)
-                }            
-            }
-        }
-    }
-
-    drawSet();
-
-    var startX = -1;
-    var startY = -1;
-    canvas.onmousedown = function (e) {
-        // startX = e.clientX - canvasRect.left;
-        // startY = e.clientY - canvasRect.top;
-        startX = (e.clientX - canvasRect.left) / canvas.clientWidth * width; console.log(startX);
-        startY = (e.clientY - canvasRect.top) / canvas.clientHeight * height; console.log(startY);
-    }
-
-    canvas.onmouseup = function (e) {
-        if (startX < 0 || startY < 0) {
-            return;
-        }
-
-        // var endX = e.clientX - canvasRect.left;
-        // var endY = e.clientY - canvasRect.top;
-
-        var endX = (e.clientX - canvasRect.left) / canvas.clientWidth * width;
-        var endY = (e.clientY - canvasRect.top) / canvas.clientHeight * height;
-
-        // console.log(startX);
-        // console.log(startY);
-        // console.log(endX);
-        // console.log(endY);
-
-        var prev = {x_min:x_min, x_max:x_max, y_min:y_min, y_max:y_max};
-        x_min = translateX(Math.min(startX, endX), prev);
-        x_max = translateX(Math.max(startX, endX), prev);
-        y_min = translateY(Math.max(startY, endY), prev);
-        y_max = translateY(Math.min(startY, endY), prev);
-        
-        // console.log(x_min);
-        // console.log(x_max);
-        // console.log(y_min);
-        // console.log(y_max);
-
-        maxIter *= 2;
-        clearCanvas();
-        drawSet();
-
-        startX = -1;
-        startY = -1;
-    }
+    var canvasManager = new CanvasManager('main', defaults, checkJuliaPoint);
+    canvasManager.drawSet();
 }
